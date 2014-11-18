ff7b051da6db9d02e30fd434173a2d30628f802d
Pablo <pablo@pabloCorp.es>
2014-11-18 11:09:57 +0100

    Añadiendo el segundo cambio. Después de crear branch

--- projectGIT/main.cpp
+++ projectGIT/main.cpp
@@ -6,6 +6,7 @@ int main(int argc, char *argv[])
     QCoreApplication a(argc, argv);
 
     qDebug() << "Esto es parte de la rama inicial antes de crear el Branch.";
+    qDebug() << "Esto es parte de la rama inicial después de crear el Branch.";
 
     return a.exec();
 }
