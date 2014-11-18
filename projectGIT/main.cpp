ae6d23810830575b0a661c8ac3c5fd71ed80b986
Pablo <pablo@pabloCorp.es>
2014-11-18 11:05:22 +0100

    Añadiendo el primer cambio antes del branch

--- projectGIT/main.cpp
+++ projectGIT/main.cpp
@@ -1,8 +1,11 @@
 #include <QCoreApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
     QCoreApplication a(argc, argv);
+
+    qDebug() << "Esto es parte de la rama inicial antes de crear el Branch.";
 
     return a.exec();
 }
